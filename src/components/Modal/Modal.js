817ceb8306859e826894ccd5de6fc726a6d1e26e
corey decato <corey.decato@richmond.edu>
2018-08-18 19:01:32 -0400

    modal changes

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,13 +5,14 @@ const Modal = props => (
     <div className="modal" id="noteModal" tabIndex="-1" role="dialog" aria-labelledby="noteModalLabel" aria-hidden="false">
         <div className="modal-dialog" role="document">
         <div className="modal-content">
-            <div className="modal-header">
+            {/* <div className="modal-header">
             <h4 className="modal-title" id="noteModalLabel">Modal title</h4>
-            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="false">&times;</span>
-            </button>
-            </div>
+            
+            </div> */}
             <div className="modal-body">
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="false">&times;</span>
+                </button>
                 <div id="sign-in" className="col">
                     <h2>Sign In</h2>
                     <form id="sign-in-form">
@@ -19,14 +20,23 @@ const Modal = props => (
                         <input type="text" className="form-control" id="sign-in-username"
                         placeholder="Username/Email"></input>
                         <input type="password" className="form-control" id="sign-in-password" placeholder="Password"></input>
-                        <button type="submit" className="btn btn-success submit btn-md">Sign In</button>
+                        <button type="submit" className="btn btn-danger submit btn-md">Sign In</button>
                         </div>
                     </form>
                 </div>
             </div>
             <div className="modal-footer">
-            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button id="save-note" type="button" className="btn btn-primary" data-id="" data-dismiss="modal">Save changes</button>
+                <div id="sign-up" className="col">
+                    <h2>Sign Up</h2>
+                    <form id="sign-up-form">
+                        <div className="form-group">
+                        <input type="text" className="form-control" id="sign-up-username"
+                        placeholder="Username/Email"></input>
+                        <input type="password" className="form-control" id="sign-up-password" placeholder="Password"></input>
+                        <button type="submit" className="btn btn-danger submit btn-md">Sign Up</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
         </div>
