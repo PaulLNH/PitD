3c5524fbf194811a04fe150450e597abde69d1e8
Ian Videyko <ian.videyko@gmail.com>
2018-08-20 14:00:20 -0400

    initial upload for login

--- server.js
+++ server.js
@@ -1,233 +1,36 @@
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io').listen(server);
-const _ = require('lodash');
+const express = require("express");
+const bodyParser = require("body-parser");
+const jwt = require('jwt-express');
+const env = process.env.NODE_ENV || "development";
+const config = require(`${__dirname}/config/config.json`)[env];
+
 const PORT = process.env.PORT || 8080;
+const app = express();
+const db = require("./models");
+// Express middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.use(jwt.init(config.tokenSecret, {
+  cookies: false
+}));
+app.use((err, req, res, next) => {
+  console.log(`${req.method} ${req.url} - ${err.message}`);
 
-var games = {};
-var players = {};
-// Timer in seconds
-const maxTimePerRound = 10;
-var huntTeam = "zombie";
-const masterSpawn = [{
-        x: 163,
-        y: 92
-    },
-    {
-        x: 521,
-        y: 409
-    },
-    {
-        x: 552,
-        y: 139
-    },
-    {
-        x: 145,
-        y: 366
-    },
-    {
-        x: 375,
-        y: 112
-    },
-    {
-        x: 310,
-        y: 388
-    },
-    {
-        x: 35,
-        y: 264
-    },
-    {
-        x: 463,
-        y: 257
-    }
-]
+  if (err.name == 'JWTExpressError') err.status = 401;
 
-////////////////// TODO: 
-// - Form validation on names: min 1, max 8, name case "_.startCase(_.toLower('UseRnaMe'))" Although this should be done when we gather the players username going into the database.
-
-var timeLeft = 10;
-
-// Timer
-var countDown = function () {
-    var resurrect = [];
-
-    if (timeLeft == 0) {
-        console.log(`Resetting timer`);
-        timeLeft = maxTimePerRound;
-
-        var spawnCounter = 0;
-        Object.keys(players).forEach(function (id) {
-
-            if (players[id].alive == false) {
-                console.log(`Resurecting ${players[id].username}!`);
-                players[id].alive = true;
-                var spawn = masterSpawn[spawnCounter];
-                spawnCounter++;
-                var playerSpawn = {
-                    id: id,
-                    x: spawn.x,
-                    y: spawn.y
-                };
-                resurrect.push(playerSpawn);
-            }
-        });
-
-        switch (huntTeam) {
-            case "human":
-                huntTeam = "zombie";
-                break;
-            case "zombie":
-                huntTeam = "human";
-                break;
-            default:
-                huntTeam = "zombie";
-        }
-    } else {
-        timeLeft--;
-        // console.log(timeLeft);
-    }
-    // console.log(huntTeam);
-    console.log(resurrect);
-    io.sockets.emit('timer', {
-        timeLeft: timeLeft,
-        huntTeam: huntTeam,
-        resurrect: resurrect
-    });
-}
-
-setInterval(countDown, 1000);
-
-var scores = {
-    human: 0,
-    zombie: 0
-};
-
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
-
-io.on('connection', socket => {
-    console.log('a user connected: ', socket.id);
-
-    var getSpawn = function () {
-        let sp = masterSpawn;
-        let i = _.keys(players).length;
-        console.log(sp[i]);
-        // players(id).x = sp[i].x;
-        // players(id).y = sp[i].y;
-        return sp[i]
-    }
-    // create a new player and add it to our players object
-    // Username, Id, spawnPoint, team, 
-    players[socket.id] = {
-        // Form validation to display username as standard name case
-        username: _.startCase(_.toLower(randUsername())),
-        playerId: socket.id,
-        directionMoving: "none",
-        sp: getSpawn(),
-        alive: true,
-        team: assignTeam(),
-        speed: 100,
-        score: 0,
-        usernameText: null,
-        time: timeLeft,
-        spawnLocation: function (masterSpawn) {
-            let sp = masterSpawn;
-            let i = _.keys(players).length;
-            console.log(sp[i]);
-            return sp[i]
-        }
-    };
-    console.log(timeLeft);
-    // TODO:
-    // CANNOT USER IF STATEMENT HERE, ONLY SENDS ON CONNECTION.
-    // NEED TO FIND ANOTHER WAY TO SYNC CLIENTS TIMER
-    // if (resetTimer) {
-    //     console.log(`Emitting timer reset node`);
-    //     socket.emit('timerUpdate');
-    // }
-
-    // send the players object to the new player
-    socket.emit('currentPlayers', players);
-    // // send the current scores
-    socket.emit('scoreUpdate', scores);
-    // update all other players of the new player
-    socket.broadcast.emit('newPlayer', players[socket.id]);
-
-    // when a player disconnects, remove them from our players object
-    socket.on('disconnect', () => {
-        console.log(`${socket.id} has left the game.`);
-        delete players[socket.id];
-        // emit a message to all players to remove this player
-        io.emit('disconnect', socket.id);
-    });
-
-    // when a player moves, update the player data
-    socket.on('playerMovement', movementData => {
-        players[socket.id].directionMoving = movementData.directionMoving;
-        players[socket.id].x = movementData.x;
-        players[socket.id].y = movementData.y;
-        // emit a message to all players about the player that moved
-        socket.broadcast.emit('playerMoved', players[socket.id]);
-    });
-
-    socket.on('characterDies', id => {
-        console.log(id);
-        if (players[id.victim].alive) {
-
-            console.log(`Looks like ${id.victim} has died.`);
-            players[id.victim].alive = false;
-            io.emit('characterDied', id.victim);
-
-            players[id.attacker].score
-            if (players[id.attacker].team === 'human') {
-                scores.human += 10;
-            } else {
-                scores.zombie += 10;
-            }
-            // io.emit('scoreUpdate', {scores: scores, attackerScore: players[id.attacker].score});
-
-            // LOGIC - If update player ID to dead - If statement checking if player is alive before running scoreUpdate
-            // io.emit('characterDied', id);
-        }
-    });
-
-    // socket.on('playerTagged', headToHead => {
-    //     console.log(`Yup, server says we have collision! ${headToHead}`);
-    //     if (players[socket.id].team === 'human') {
-    //         players[socket.id].score
-    //         scores.human += 10;
-    //     } else {
-    //         scores.zombie += 10;
-    //     }
-    //     io.emit('scoreUpdate', scores);
-    // });
+  res.status(err.status || 400).send({message: `${err.message}`});
 });
 
-function randomSpawn() {
-    var spMax = _.keys(masterSpawn).length;
-    var spawn = _.values(masterSpawn);
-    spawn = spawn[getRandomInt(0, spMax)];
-    console.log(`Spawning at x: ${spawn.x}, y: ${spawn.y}`);
-    return spawn
-}
+// Api routes
+require("./api/account.js")(app);
+// require("./api/game.js")(app);
+require("./api/admin.js")(app);
+require("./api/html.js")(app);
 
-function assignTeam() {
-    var team = (Math.floor(Math.random() * 2) == 0) ? 'human' : 'zombie';
-    console.log(`on team: ${team}`);
-    return team
-}
-
-function randUsername() {
-    var name = (Math.floor(Math.random() * 2) == 0) ? 'Paul' : 'Jashan';
-    console.log(`random name: ${name}`);
-    return name
-}
-
-// function getRandomInt(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-
-server.listen(PORT, () => console.log(`Listening on ${server.address().port}`));
+// Sync sequelize and start http server
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
